1a767f0440c40f869802587a65b6395450602243
Collins Kariuki <collinsmunkary@gmail.com>
2024-07-14 10:17:55 +0300

    Database connection added. Application fully functional

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response} from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 import * as dotenv from 'dotenv';
 import { connectToDatabase } from './models/database.js';
 
@@ -8,6 +10,10 @@ const app = express();
 
 // set EJS as templating engine
 app.set('view engine', 'ejs');
+
+// Use import.meta.url to get the current module's URL
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //set path for it to work from any where
 app.set('views', path.join(__dirname, '/views'))
--- src/models/database.ts
+++ src/models/database.ts
@@ -3,7 +3,7 @@ import { Campground } from './campground.js';
 
 // An object to hold all the collections that will be created in this project
 export const collections: {
-  campground?: mongodb.Collection<Campground>
+  campgrounds?: mongodb.Collection<Campground>
 } = {};
 
 
@@ -14,5 +14,54 @@ export async function connectToDatabase(uri: string) {
 
   //create a DB instance and pass in the name of the db
   const db = client.db('yelp-camp')
+  // section for validation
+  await applySchemaValidation(db); // also create a collection
+
+  const campgroundsCollection = db.collection<Campground>("campgrounds");
+  collections.campgrounds = campgroundsCollection;
+}
+
+// Validation Schema for the collection
+async function applySchemaValidation(db: mongodb.Db) {
+  // Define the validator. Store it in constant named jsonSchema
+  // jsonSchema(The validator) is a document that is used to specify
+  // validation rules.
+  const jsonSchema = {
+    // The $jsonSchema keyword is used in MongoDB to define a JSON schema for a collection.
+    $jsonSchema: {
+      bsonType: "object",
+      required: ['title', 'price', 'description','location'],
+      additionalProperties: false,
+      properties: {
+        _id: {},
+        title: {
+          bsonType: 'string',
+          description: "'title' is a string and is required"
+        },
+        price: {
+          bsonType: 'string',
+          description: "'position is a string and is required'"
+        },
+        description: {
+          bsonType: 'string',
+          description: "'description' is a string and is required"
+        },
+        location: {
+          bsonType: 'string',
+          description: "'location' is a string and is required"
+        }
+      }
+    }
+  };
+
+  // Try applying the modification to the collection, if the collection doesn't exist, create it
+  await db.command({
+    collMod: "campgrounds",
+    validator: jsonSchema
+  }).catch(async (error: mongodb.MongoServerError) => {
+    if (error.codeName === "NamespaceNotFound") {
+      await db.createCollection("employees", {validator: jsonSchema})
+    }
+  })
 }
 
